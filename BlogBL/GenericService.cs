d46e5a59ed652c62e5f4fb7581a745d40e216dcb
Serhii <sboyko1995@gmail.com>
2020-01-06 18:57:15 +0200

    Add project files.

---
+++ BlogBL/GenericService.cs
@@ -0,0 +1,69 @@
+﻿using BlogDAL.Repository;
+using System.Collections.Generic;
+
+namespace BlogBL
+{
+    public interface IGenereicService<BLModel>
+        where BLModel : class
+       
+    {
+        BLModel FindById(int id);
+        IEnumerable<BLModel> GetAll();
+        void AddItem(BLModel bLModel);
+        void Delete(BLModel deleteItem);
+        void Update(BLModel updateBLItem);
+
+    }
+
+    public abstract class GenericService<BLModel, DModel> : IGenereicService<BLModel>
+        where BLModel : class
+        where DModel : class
+    {
+        private readonly IGenericRepository<DModel> _repositroy;
+        public GenericService(IGenericRepository<DModel> repository)
+        { 
+            _repositroy = repository;
+        }
+
+        public void AddItem(BLModel newBLModel)
+        {
+            DModel newDLModel = Map(newBLModel);
+            _repositroy.Create(newDLModel);
+        }
+
+        public void Delete(BLModel deleteItem)
+        {
+            DModel deleteDLModel = Map(deleteItem);
+            _repositroy.Remove(deleteDLModel);
+        }
+
+        public void Update(BLModel updateBLItem)
+        {
+            DModel updateDLModel = Map(updateBLItem);
+            _repositroy.Update(updateDLModel);
+        }
+
+        public virtual BLModel FindById(int id)
+        {
+            var entity = _repositroy.FindById(id);
+            return Map(entity);
+        }
+
+        public IEnumerable<BLModel> GetAll()
+        {
+            var listEntity = _repositroy.Get();           
+            return Map(listEntity);
+        }
+
+        /// <summary>
+        /// convert Dal model to Bl molder
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public abstract BLModel Map(DModel entity);
+        public abstract DModel Map(BLModel blmodel);
+
+        public abstract IEnumerable<BLModel> Map(IList<DModel> entity);
+        public abstract IEnumerable<DModel> Map(IList<BLModel> entity);
+    }
+}
